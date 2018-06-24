d32dc8aa21b2244501ad65abd83b4bd83a143a2c
oejho <uj.ikhwan@gmail.com>
2018-06-24 22:00:18 +0700

    file baru

--- src/main/java/id/cipta/dwi/karya/ciptadwikarya/service/CustomersService.java
+++ src/main/java/id/cipta/dwi/karya/ciptadwikarya/service/CustomersService.java
@@ -1,10 +1,19 @@
 package id.cipta.dwi.karya.ciptadwikarya.service;
 
 import id.cipta.dwi.karya.ciptadwikarya.domain.Customers;
+import java.util.List;
 
 
 public interface CustomersService {
    
     public Customers saveCustomer(Customers customers);
+    
+    public Customers updateCustomers(Customers customers);
+    
+    List<Customers> findAll();
+    
+    Customers findOne(Integer id);
+    
+    void deleteCustomer(Integer id);
     
 }
--- src/main/java/id/cipta/dwi/karya/ciptadwikarya/service/InventoryServiceImpl.java
+++ src/main/java/id/cipta/dwi/karya/ciptadwikarya/service/InventoryServiceImpl.java
@@ -2,11 +2,14 @@ package id.cipta.dwi.karya.ciptadwikarya.service;
 
 import id.cipta.dwi.karya.ciptadwikarya.domain.Inventory;
 import id.cipta.dwi.karya.ciptadwikarya.repository.InventoryRepository;
+import java.util.List;
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Service
 public class InventoryServiceImpl implements InventoryService{
@@ -26,6 +29,37 @@ public class InventoryServiceImpl implements InventoryService{
                    
         }        
         return inventory;        
+    }
+
+    @Override
+    public Inventory updateInventory(@Valid @ModelAttribute("inventories") Inventory inventories) {
+        logger.info("Result inventory: "+inventories.toString());
+        if(inventories != null){
+            Inventory inventoryDb = repository.findByIdInventory(inventories.getIdInventory());
+            inventoryDb.setName(inventories.getName());
+            inventoryDb.setSumIn(inventories.getSumIn());
+            inventoryDb.setSumOut(0);
+            inventoryDb.setSumEnd(0);
+            inventoryDb.setNote(inventories.getNote());
+            repository.save(inventoryDb);
+            logger.info(inventoryDb.toString());        
+        }        
+        return inventories;   
+    }
+
+    @Override
+    public List<Inventory> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Inventory findOne(Integer idInventory) {
+       return repository.findOne(idInventory);
+    }
+
+    @Override
+    public void deleteInventory(Integer idInventory) {
+        repository.delete(idInventory);
     }
     
 }
